147dd194ad8cb5c12a2bc2007814e3db0e12c3ee
Peter-Marc Ricketts <pricketts@singer.com.jm>
2022-05-03 11:21:04 -0500

    Add project files.

---
+++ FaceyPhotos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FaceyPhotos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
