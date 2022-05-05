abd2e446a4fc506df3e582694f87b7eb05664a24
Peter-Marc Ricketts <pricketts@singer.com.jm>
2022-05-04 21:08:29 -0500

    Layout changes

--- FaceyPhotos/Data/ApplicationDbContext.cs
+++ FaceyPhotos/Data/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
 
 namespace FaceyPhotos.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<Customer>
+    public class ApplicationDbContext : IdentityDbContext<Customer>       
+      
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+    }
+
+    public class CalenderContext : DbContext
+    {
+        public CalendarContext() : base()
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<CalendarContext>());
+        }
+        public System.Data.Entity.DbSet<AppointmentCalendar.Models.Appointment> Appointments { get; set; }
     }
 }
